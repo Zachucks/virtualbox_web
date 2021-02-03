b507a4f4d7da0125f135b29fddf0729d0d71e9ba
Zachariah Huckstep <zachucks@hotmail.com>
2021-02-03 14:18:48 -0500

    Added navbar and fixed style references

--- styles.php
+++ styles.php
@@ -3,5 +3,5 @@
 		"bootstrap.css",
 		"styles.css"
 	];
-	foreach ($css_files as $cssf) { $v = rand(11111,99999);echo "<script type='text/javascript' src='${cssf}?v=${v}'></script>"; }
+	foreach ($css_files as $cssf) { $v = rand(11111,99999);echo "<link rel='stylesheet' type='text/css' href='${cssf}?v=${v}'>"; }
 ?>
